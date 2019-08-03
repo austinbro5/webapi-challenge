a3568331f0730d3adf1c7cff70e3c9297dd152b7
austin breaux <47146701+austinbro5@users.noreply.github.com>
2019-08-02 20:22:53 -0500

    set up the server

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const express = require('express')
+const projectRoutes = require('./projects/projectRoutes')
+const actionRoutes = require('./actions/actionRoutes')
+const mwConfig = require('./data/mwConfig')
+
+const PORT = 5000
+const server = express()
+
+mwConfig(server)
+
+server.use('/api/projects', projectRoutes)
+server.use('/api/actions', actionRoutes)
+
+server.listen(PORT, () => console.log(`API running on port ${PORT}`))
