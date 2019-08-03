7b3a080868ffb04f8cad095bd2c81a89c44e2325
austin breaux <47146701+austinbro5@users.noreply.github.com>
2019-08-02 20:25:09 -0500

    added get routes

--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -1,3 +1,51 @@
 const express = require('express')
 const db = require('../data/helpers/projectModel')
 const router = express.Router()
+
+// Get Routes //
+
+router.get('/', (req, res) => {
+    db.get()
+      .then(projects => {
+        res.json(projects)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: 'The project information could not be retrieved' })
+      })
+  })
+  
+  router.get('/:id', (req, res) => {
+    const { id } = req.params
+    db.get(id)
+      .then(projects => {
+        if (projects) {
+          res.json(projects)
+        } else {
+          res.status(404).json({ message: 'project does not exist' })
+        }
+      })
+      .catch(err => {
+        res.status(404).json({
+          error: 'Information about the project could not be retrieved.'
+        })
+      })
+  })
+  
+  router.get('/:id/actions', (req, res) => {
+    const { id } = req.params
+    db.getProjectActions(id)
+      .then(actions => {
+        if (actions) {
+          res.json(actions)
+        } else {
+          res.status(404).json({ message: 'Action does not exist' })
+        }
+      })
+      .catch(err => {
+        res
+          .status(404)
+          .json({ error: 'Info about this action could not be retrieved' })
+      })
+  })
